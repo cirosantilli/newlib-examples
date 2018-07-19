2286f1496813a99619480ab96ea22b7974e6b25c
Ciro Santilli <ciro.santilli@gmail.com>
2018-07-19 21:20:32 +0100

    cleanup a bit more

--- main.c
+++ main.c
@@ -6,13 +6,13 @@ void main() {
     char *ptr = NULL;
     size_t alloc_size = 1;
     while (1) {
-        printf("enter a character: ");
+        printf("enter a character\n");
         c = getchar();
         printf("got: %c\n", c);
         ptr = realloc(ptr, alloc_size);
-        if(ptr == NULL) {
-            puts("Out of memory!\nProgram halting.");
-            for(;;);
+        if (ptr == NULL) {
+            puts("out of memory");
+            break;
         } else {
             printf("new alloc of %d bytes at address 0x%p\n", alloc_size, ptr);
             alloc_size <<= 1;
