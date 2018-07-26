28a65ced1e63c72a6c42f342f94e21fa0dbd9774
Ciro Santilli <ciro.santilli@gmail.com>
2018-07-26 09:20:50 +0100

    gem5: add to new section

--- syscalls.c
+++ syscalls.c
@@ -9,18 +9,15 @@ enum {
 #define UART_DR(baseaddr) (*(unsigned int *)(baseaddr))
 #define UART_FR(baseaddr) (*(((unsigned int *)(baseaddr))+6))
 
-
 int _close(int file) { return -1; }
 
 int _fstat(int file, struct stat *st) {
-            st->st_mode = S_IFCHR;
-            return 0;
-          }
+    st->st_mode = S_IFCHR;
+    return 0;
+}
 
 int _isatty(int file) { return 1; }
-
 int _lseek(int file, int ptr, int dir) { return 0; }
-
 int _open(const char *name, int flags, int mode) { return -1; }
 
 int _read(int file, char *ptr, int len) {
@@ -40,30 +37,26 @@ int _read(int file, char *ptr, int len) {
 
 char *heap_end = 0;
 caddr_t _sbrk(int incr) {
-            extern char heap_low; /* Defined by the linker */
-            extern char heap_top; /* Defined by the linker */
-            char *prev_heap_end;
-          
-            if (heap_end == 0) {
-              heap_end = &heap_low;
-            }
-            prev_heap_end = heap_end;
-            
-            if (heap_end + incr > &heap_top) {
-                /* Heap and stack collision */
-                return (caddr_t)0;
-            }
-          
-            heap_end += incr;
-            return (caddr_t) prev_heap_end;
-          }
+    extern char heap_low;
+    extern char heap_top;
+    char *prev_heap_end;
+    if (heap_end == 0) {
+        heap_end = &heap_low;
+    }
+    prev_heap_end = heap_end;
+    if (heap_end + incr > &heap_top) {
+        /* Heap and stack collision */
+        return (caddr_t)0;
+    }
+    heap_end += incr;
+    return (caddr_t)prev_heap_end;
+}
 
 int _write(int file, char *ptr, int len) {
-            int todo;
-          
-            for (todo = 0; todo < len; todo++) {
-                UART_DR(UART0_ADDR) = *ptr++;
-            }
-            return len;
-          }
+    int todo;
+    for (todo = 0; todo < len; todo++) {
+        UART_DR(UART0_ADDR) = *ptr++;
+    }
+    return len;
+}
 
